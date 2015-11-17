7d85e6b6f9b16bd2380e999c2c6b47a1d18f8dc1
Ethan.Zhang <OO.Snail@Gmail.com>
2015-11-17 11:53:57 +0800

    根据原版本进行了修改，具体修改内容见README.MD中"修改".

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# RabbitMQ cluster
+# RabbitMQ Docker 集群
+> 从jrlangford的[docker_rabbitmq_cluster](https://github.com/jrlangford/docker_rabbitmq_cluster)克隆而来,并做了简单修改.
 
-In this example a RabbitMQ cluster is created
+### 说明
+使用DNS的原因是: Docker无法识别RabbitMQ节点,直接修改hosts文件可行但维护麻烦.
 
-To launch the cluster run `. launch.sh`
-
-You can send messages to this cluster trhough the
-IP and NODE_PORT appearing in the output.
+### 修改
+1. 添加并引用ERLANG_COOKIE变量,从基础镜像rabbitmq中创建相同Cookie的节点.省去单独编译一个镜像作为集群的基础镜像,jrlangford/rabbitmq. 
+2. 创建RabbitMQ节点时增加"p $AMQP_PORT:5672 \",用于将amqp协议端口绑定至宿主机,否则无法访问.
+3. 将DNS域名提为变量,便于更改为主机喜欢的名称.
--- launch.sh
+++ launch.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
-NODE1=j1
-NODE2=j2
-NODE3=j3
+NODE1=rabbitmq1
+NODE2=rabbitmq2
+NODE3=rabbitmq3
+ERLANG_COOKIE='YZSDHWMFSMKEMBDHSGGZ'
+DNS_NAME=localdomain
 
 echo "Launching dns resolver"
-docker run -d --name dns -v /var/run/docker.sock:/docker.sock phensley/docker-dns --domain rabbit.com
+docker run -d --name dns -v /var/run/docker.sock:/docker.sock phensley/docker-dns --domain $DNS_NAME
+sleep 3
 
 echo "Launching nodes"
 function launch_node {
 	NODE=$1
-	MGMT_PORT=$2
+	AMQP_PORT=$2
+	MGMT_PORT=$3
 	HOST=${NODE}-host
 	docker run -d \
         	--name=$NODE \
+        	-p $AMQP_PORT:5672 \
         	-p $MGMT_PORT:15672 \
         	-e RABBITMQ_NODENAME=$NODE \
-        	-h ${NODE}.rabbit.com \
+			-e RABBITMQ_ERLANG_COOKIE=$ERLANG_COOKIE \
+        	-h ${NODE}.$DNS_NAME \
         	--dns $(docker inspect -f '{{.NetworkSettings.IPAddress}}' dns) \
-        	--dns-search rabbit.com \
-		jrlangford/rabbitmq:cluster-ready
+        	--dns-search $DNS_NAME \
+		rabbitmq:3.5-management
 }
 
-launch_node $NODE1 15672
-launch_node $NODE2 15673
-launch_node $NODE3 15674
+launch_node $NODE1 5672 15672
+launch_node $NODE2 5673 15673
+launch_node $NODE3 5674 15674
 
 echo "Sleeping to allow time for initialisation"
 sleep 3
@@ -35,7 +41,7 @@ docker exec $NODE2 bash -c \
 	rabbitmqctl start_app" &
 docker exec $NODE3 bash -c \
 	"rabbitmqctl stop_app && \
-	rabbitmqctl join_cluster $NODE1@$NODE1 && \
+	rabbitmqctl join_cluster --ram $NODE1@$NODE1 && \
 	rabbitmqctl start_app" &
 
 wait
@@ -46,5 +52,4 @@ docker exec $NODE1 rabbitmqctl set_policy HA '^(?!amq\.).*' '{"ha-mode": "all"}'
 echo
 echo "Finished, cluster running!!!"
 echo
-echo "IP:NODE_PORT - $(docker inspect -f '{{.NetworkSettings.IPAddress}}' $NODE1):5672"
 echo "RabbitMQ Management Console - localhost:15672"
